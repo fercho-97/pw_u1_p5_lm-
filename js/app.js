6508424b056bc993537c1ad3d233591f1d3fd5b3
Fernando Mosquera <luismosquera97@gmail.com>
2023-12-09 10:36:46 -0500

    Options API

--- js/app.js
+++ js/app.js
@@ -2,19 +2,47 @@ console.log("elementos Vue");
 
 console.log(Vue);
 
-const app=Vue.createApp({
-   /* template: `
-    <h1>  Hola Mundo </h1>
-    <p> Desde App.js </p>
+const app = Vue.createApp({
+    /* template: `
+     <h1>  Hola Mundo </h1>
+     <p> Desde App.js </p>
+ 
+     <h6>{{1+1}}</h6>
+ 
+     <p>  {{[1,2,3,4,5,6,7,8]}} </p>
+ 
+     <p> {{true? 'Verdadero': 'Falso'}}  </p>
+     <p> {{false? 'Verdadero': 'Falso'}}  </p>
+     `
+ */
+    methods: {
+        cambiarMensaje(){
+            console.log("cambiar mensaje");
+            console.log(this.mensaje);
+            this.mensaje='valor cambiado';
+        
+        },
+        cambiarNumero(){
+            console.log("cambiar valor");
+            console.log(this.valor);
+            this.valor++;
+            
 
-    <h6>{{1+1}}</h6>
+        }
 
-    <p>  {{[1,2,3,4,5,6,7,8]}} </p>
+    },
+    watch: {
 
-    <p> {{true? 'Verdadero': 'Falso'}}  </p>
-    <p> {{false? 'Verdadero': 'Falso'}}  </p>
-    `
-*/
+    },
+
+    data() {
+
+        return {
+            mensaje: 'Hola mundo desde VUe.JS',
+            valor: 100
+
+        }
+    }
 
 });
 
