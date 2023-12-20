52cc310acc7d849b216fd3c5ab6a9d5dd1668947
Fernando Mosquera <luismosquera97@gmail.com>
2023-12-20 18:12:29 -0500

    formulario con vue

--- js/estudianteApp.js
+++ js/estudianteApp.js
@@ -1,7 +1,33 @@
 const estudiantes = [];
-
+console.table(estudiantes);
 const app = Vue.createApp({
+    methods: {
+        agregarEstudiante() {
+            console.log('agregando estudiante');
+            //const estu = { nombre: this.nombre, apellido: this.apellido };
+            //this.lista.unshift(estu);
+            //this.lista.push(estu);
+            this.lista.push({ nombre: this.nombre, apellido: this.apellido, hobby:this.hobby, genero: this.genero, pais: this.pais})
+        }
 
+    },
+    watch: {
+
+    },
+    data(){
+
+        return{
+            lista: estudiantes,
+            nombre: null, //undefined
+            apellido: null,//undefined
+            hobby: null, //undefined
+            genero: null,//undefined
+            pais: null, //undefined
+            
+
+        }
+
+    }
 
 });
 
